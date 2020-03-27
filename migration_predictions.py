9b55ce42fa354a04272ea154305c7770aa7edfdc
Matthew Preston <46906782+PrestonMatt@users.noreply.github.com>
2020-03-27 11:50:07 -0400

    Structure of predictions

---
+++ migration_predictions.py
@@ -0,0 +1,15 @@
+#https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+from sklearn.linear_model import LogisticRegression as lr
+#https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+from sklearn.neighbors import KNeighborsClassifier as knn
+
+print('import complete')
+
+def input_data():
+    return None
+
+def knn_migration(data):
+    return None
+
+def lr_migration(data):
+    return None
