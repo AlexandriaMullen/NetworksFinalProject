e73cbae0e7ed634b1889c75edca488a943ef46a3
Matthew Spana <spanam@bc.edu>
2020-04-02 17:47:35 -0400

    Finished parse_new_data.py

--- parse_new_data.py
+++ parse_new_data.py
@@ -1,56 +1,80 @@
 import matplotlib as mp
-import numpy as np
 import pandas as pd
 import tkinter as tk
+import numpy as np
 from tkinter import filedialog
+
 
 def decide_which_countries():
     global df
-    import_file_path = filedialog.askopenfilename()
-    xl = pd.ExcelFile(import_file_path)
+    xl = pd.ExcelFile("./data/UN_MigFlow_Totals.xlsx")
     df = xl.parse('Totals')
     data = df.to_numpy()
-    
+
     list_of_countries = []
-    
-    #remove countries with lower data counts
-    #i.e. if they don't have any data up to 1985
+
+    # remove countries with lower data counts
+    # i.e. if they don't have any data up to 1985
     for datum in data:
-        #test: print(datum),
+        # test: print(datum),
         for x in range(10):
-            #if there's in integer in the first 9 columns
-            #keeping in mind the first four are the names of the countries, etc
-            #then add that country to the list we'll keep
-            if( (isinstance(datum[x], int) or isinstance(datum[x], float))
-                and not np.isnan(datum[x])):
-                #test: print(datum[x])
-                list_of_countries.append(datum[0])
-                #no need to check the next four if applicable
+            # if there's in integer in the first 9 columns
+            # keeping in mind the first four are the names of the countries, etc
+            # then add that country to the list we'll keep
+            if ((isinstance(datum[x], int) or isinstance(datum[x], float))
+                    and not np.isnan(datum[x])):
+                # test: print(datum[x])
+                if (datum[0] == "United Kingdom of Great Britain and Northern Ireland"):
+                    list_of_countries.append(("United Kingdom", datum[1]))
+                else:
+                    list_of_countries.append((datum[0], datum[1]))
+                # no need to check the next four if applicable
                 continue
 
-    #kill duplicates:
+    # kill duplicates:
     list_of_countries = list(set(list_of_countries))
 
-    #print(list_of_countries)
+    # print(list_of_countries)
 
     try:
-        list_of_countries.remove('CntName')
+        list_of_countries.remove(('CntName', 'Criteria'))
     except ValueError:
         print('Wrong File!!')
-    
+
     return list_of_countries
 
-def make_migrant_table(excelPath):
+
+def make_migrant_table():
     countries = decide_which_countries()
 
     data_by_country = []
 
-    for country in countries:
+    for country, criteria in countries:
 
+        excelPath = "./data/" + country + ".xlsx"
         xl = pd.ExcelFile(excelPath)
-        df = xl.parse()
+        # we need the Australia, Germany, US, (etc) by Residence tab:
+        df = None
+        if country == "United States of America":
+            df = xl.parse("USA by " + criteria)
+        else:
+            df = xl.parse(country + " by " + criteria)
+
         data = df.to_numpy()
 
-        data_by_country.append([country,data])
-                
-    
+        data = country_data(country, data)
+
+        data_by_country.append([country, data])
+
+    return data_by_country
+
+
+# Given string country's name and nparr data
+# pick out the important bits
+def country_data(country, data):
+    new_data = []
+    for datum in data:  # data is list of rows
+        if (datum[0] == 'Immigrants'):
+            row = [[datum[2] + 'to ' + country], [datum[9:42]]]  # dub check 42 or 43
+            new_data.append(row)
+    return new_data
