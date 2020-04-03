a76aabb82a4dfd2845fba387b6b257e76bc8e16a
Matthew Spana <spanam@bc.edu>
2020-04-03 18:45:40 -0400

    Split migration_predictions.py into two files, make_predictions.py for main() function, and migration_predictions.py for regression models. Updated parse_new_data.py with minor changes, including spacing in beginning of comments.

--- make_predictions.py
+++ make_predictions.py
@@ -1,4 +1,15 @@
-import parse_new_data
+import parse_new_data as pnd
+
+def trim_data(X,imm_data):
+    new_imm_data = []
+    new_years = []
+    for index in range(len(imm_data)):
+        if(imm_data[index] == '..'):
+            continue
+        else:
+            new_imm_data.append(imm_data[index])
+            new_years.append(X[index])
+    return new_imm_data, new_years
 
 def main():
     # IDEA:
@@ -9,7 +20,8 @@ def main():
     # make predictions!!
 
     print("These are the following countries:")
-    print(parse_new_data.decide_which_countries())
+    countries = pnd.decide_which_countries()
+    print(countries)
     cnt_to_cnt = input(
         "From which country to which country would you like to predict?\n Input should be typed as \'CountryName to CountryName\'.\n")
 
@@ -20,11 +32,13 @@ def main():
     imm_data = []
 
     # from parse data:
-    data = parse_new_data.make_migrant_table()
+    print("Fetching " + country + " ")
+    data = pnd.make_migrant_table(countries)
+    print("Data fetched.")
     for country_and_immigration_data in data:
-        if (country_and_immigration_data[1] == cnts[1]):
+        if country_and_immigration_data[0] == cnts[1]:
             print(country_and_immigration_data)
-        #   for incoming_country in country_and_immigration_data:
+        #    for source_country_and_immigration_data in country_and_immigration_data[1]:
         #       imm_data = incoming_country[1]
         #       print(incoming_country)
     # print(imm_data)
--- parse_new_data.py
+++ parse_new_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 
+
 def decide_which_countries():
     global df
     # import_file_path = filedialog.askopenfilename()
@@ -11,11 +12,11 @@ def decide_which_countries():
     xl = pd.ExcelFile("./data/UN_MigFlow_Totals.xlsx")
     df = xl.parse('Totals')
     data = df.to_numpy()
-    
+
     list_of_countries = []
     # list_of_criteria = []
     # both = []
-    
+
     # remove countries with lower data counts
     # i.e. if they don't have any data up to 1985
     for datum in data:
@@ -24,12 +25,12 @@ def decide_which_countries():
             # if there's in integer in the first 9 columns
             # keeping in mind the first four are the names of the countries, etc
             # then add that country to the list we'll keep
-            if( (isinstance(datum[x], int) or isinstance(datum[x], float))
-                and not np.isnan(datum[x])):
+            if ((isinstance(datum[x], int) or isinstance(datum[x], float))
+                    and not np.isnan(datum[x])):
                 # test: print(datum[x])
                 if (datum[0] == "United Kingdom of Great Britain and Northern Ireland"):
                     list_of_countries.append("United Kingdom")
-                elif(datum[0] == 'CntName' or datum[2] == 'Emigrants'):
+                elif (datum[0] == 'CntName' or datum[2] == 'Emigrants'):
                     continue
                 else:
                     list_of_countries.append(datum[0])
@@ -47,14 +48,13 @@ def decide_which_countries():
         list_of_countries.remove('CntName')
     except ValueError:
         print('List of Countries gathered.')
-    
+
     # print(list_of_countries)
 
     return list_of_countries
 
-def make_migrant_table():
-    countries = decide_which_countries()
 
+def make_migrant_table(countries):
     criteria = ['Residence', 'Citizenship', 'Place of birth']
 
     data_by_country = []
@@ -62,7 +62,7 @@ def make_migrant_table():
     for country in countries:
 
         # print("Country: ", country, "|", end = " ")
-        
+
         excelPath = "./data/" + country + ".xlsx"
         xl = pd.ExcelFile(excelPath)
 
@@ -71,31 +71,31 @@ def make_migrant_table():
         # print(tabnames)
         # want residence, then citizenship, then pob
         for tabname in tabnames:
-            if(tabname == country + " by " + criteria[0]):
+            if (tabname == country + " by " + criteria[0]):
                 parsename = country + " by " + criteria[0]
-            elif(tabname == country + " by " + criteria[1]):
+            elif (tabname == country + " by " + criteria[1]):
                 parsename = country + " by " + criteria[1]
-            elif(tabname == country + " by " + criteria[2]):
+            elif (tabname == country + " by " + criteria[2]):
                 parsename = country + " by " + criteria[2]
 
         # we need the Australia, Germany, US, (etc) by Residence tab:
         # but the tab is different for each thing
         df = None
         try:
-            if(country is 'United States of America'):
+            if (country is 'United States of America'):
                 df = xl.parse("USA by " + criteria[2])
-            if(country == 'United Kingdom'):
+            if (country == 'United Kingdom'):
                 df = xl.parse("United Kingdom by " + criteria[0])
             else:
                 df = xl.parse(parsename)
         except:
-            df = xl.parse("USA by " + criteria[2]) # USA fails sometimes because python string checking sucks
+            df = xl.parse("USA by " + criteria[2])  # USA fails sometimes because python string checking sucks
 
         data = df.to_numpy()
-            
-        data = country_data(countries,data)
 
-        cnt = [country,data]
+        data = country_data(countries, data)
+
+        cnt = [country, data]
 
         # We want a table like this:
         # [ [Country1, [ [countr2, [data...]], [countr3, [data...]], ... ], ... ]
@@ -103,13 +103,18 @@ def make_migrant_table():
 
     return data_by_country
 
+
 # Given string country's name and nparr data
 # pick out the important bits
-def country_data(countries,data):
+def country_data(countries, data):
     new_data = []
-    for datum in data: #data is list of rows
-        if(datum[0] == 'Immigrants' and datum[2] in countries):
-            row = [datum[2],datum[9:43]] #dub check 42 or 43 (DONE)
-            #print(row)
+    for datum in data:  # data is list of rows
+        if (datum[0] == 'Immigrants' and datum[2] in countries):
+            row = [datum[2], datum[9:43]]  # dub check 42 or 43 (DONE)
+            # print(row)
             new_data.append(row)
     return new_data
+
+
+def main():
+    print(make_migrant_table(decide_which_countries()))
